1a7b095cf4fe3e094edc64c2e2eff95f2c0b4a6f
Vladimir Guguiev <wizardzloy@gmail.com>
2021-01-10 23:38:42 +0100

    (cargo-release) version 0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/doctor/0.2.2")]
+#![doc(html_root_url = "https://docs.rs/doctor/0.3.0")]
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 
 pub mod ast;
